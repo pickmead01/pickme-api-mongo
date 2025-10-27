69bf0fe43a7c1b1046026e214d043aca06b4cfbb
bobzeng <bob.zeng@bviwit.com>
2025-10-27 11:53:41 +0800

    fix bug

--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -379,7 +379,7 @@ categoryRouter.post(
         headDescription,
         name,
         keyName,
-        upperCategory: upperCategory.name,
+        upperCategory: upperCategory ? upperCategory.name : null,
         manualUrl,
       });
 
